fe693c84a21cce14b6d6e3eca8ec8b131f443352
Anne-Dionido <anne.dionido@telus.com>
2022-11-21 16:23:31 -0700

    Updated

--- takeUntil.js
+++ takeUntil.js
@@ -1,34 +1,31 @@
 const assertArraysEqual = function(actual, expected) {
-
-  if (eqArrays(actual, expected)) {
-    console.log(`:) Assertion Passed: ${actual} === ${expected}`);
+  if (eqArrays(actual, expected) === true) {
+    console.log(`✅✅✅ Assertion Passed: "${actual}" === "${expected}"`);
   } else {
-    console.log(`:( Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
   }
 };
 
-const eqArrays = function(actual, expected) {
-  if (actual.length !== expected.length) {
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
     return false;
-  } else {
-    for (let i = 0; i < actual.length; i++) {
-      if (actual[i] !== expected[i]) {
-        return false;
-      }
-    }
   }
-  return true;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  } return true;
 };
 
 const takeUntil = function(array, callback) {
-  const returnArray = [];
-  for (const item of array) {
-    if (!callback(item)) {
-      returnArray.push(item);
-    } else {
-      return returnArray;
+  let results = [];
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i])) {
+      results = array.slice(0, i);
+      return results;
     }
   }
+  return results;
 };
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
@@ -40,3 +37,8 @@ console.log('---');
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
+
+assertArraysEqual(results1, data1.slice(0, 5));
+assertArraysEqual(results2, data2.slice(0, 4));
+
+module.exports = takeUntil;
