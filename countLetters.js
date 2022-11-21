9b17d6d03832abdda2b97fbe9c2cb10312b69799
Anne-Dionido <anne.dionido@telus.com>
2022-11-21 16:10:08 -0700

    commit CL

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(str) {
+  let counter = {};
+
+  for (let value of str) {
+    if (value === ' ') {
+      continue;
+    } else {
+      counter[value] = (counter[value] || 0) + 1;
+    }
+  }
+  return counter;
+};
+countLetters("Anne is a student at LHL");
+console.log(countLetters("Anne is a student at LHL"));
+
+module.exports = countLetters;
