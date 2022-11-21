f1d617cf6da00aa5a81cd58beff46ef5ca60489c
Anne-Dionido <anne.dionido@telus.com>
2022-11-21 15:38:28 -0700

    lotide changes commit

---
+++ findKey.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`:) Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`:( Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = (object, callback) => {
+  for (const key in object) {
+    console.log(object[key]);
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+};
+
+const results1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2);
---
+++ head.js
@@ -0,0 +1,15 @@
+function average(list) {
+  var sum = 0;
+
+  for (var num of list) {
+    sum ++ for (const key in object) {
+	if (Object.hasOwnProperty.call(object, key)) {
+		const element = object[key];
+		num
+	}
+}
+  }
+
+  return sum / list.length;
+
+console.log(average([3, 5, 7]))}
